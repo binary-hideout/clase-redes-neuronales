e4d4b596f647d20bdb82bb70057a83619229c3e1
netotz <neto.otz@hotmail.com>
2020-03-05 10:38:58 -0600

    Agregar función para obtener lista de primos

--- practicas/primos.py
+++ practicas/primos.py
@@ -5,14 +5,19 @@ Desarrollar un programa que encuentre todos los números primos del 1 a n.
 import sys
 
 def is_prime(n, primes):
-    for p in primes[1:]:
+    for p in primes:
         if n % p == 0:
             return False
     return True
 
-n = int(sys.argv[1])
+def get_primes(n):
+    primes = [2]
+    for i in range(3, n + 1, 2):
+        if is_prime(i, primes[1:]):
+            primes.append(i)
+    return primes
 
-primes = [2]
-for i in range(3, n + 1, 2):
-    if is_prime(i, primes):
-        primes.append(i)
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    primes = get_primes(n)
+    print(primes)
